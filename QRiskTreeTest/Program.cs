cdde2162b04f7ae9bb11a4ade2d1807fce1aa7c5
Simone Curzi <simone.curzi@microsoft.com>
2025-06-26 17:05:59 +0200

    Minor change

--- QRiskTreeTest/Program.cs
+++ QRiskTreeTest/Program.cs
@@ -1,72 +1,72 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using QRiskTree.Engine;
-using QRiskTree.Engine.ExtendedOpenFAIR;
-using QRiskTree.Engine.OpenFAIR;
+using ExtendedOpenFAIR;
+using OpenFAIR;
 
 var model = RiskModel.Instance;
 
 var risk1 = model.AddRisk("Risk 1: Unauthorized access to sensitive data.");
-risk1.Add(new LossEventFrequency() { Perc10 = 1, Mode = 12, Perc90 = 120, Confidence = QRiskTree.Engine.Confidence.Moderate });
-risk1.Add(new LossMagnitude() { Perc10 = 100000, Mode = 500000, Perc90 = 20000000, Confidence = QRiskTree.Engine.Confidence.Moderate});
+risk1.Add(new LossEventFrequency() { Perc10 = 1, Mode = 12, Perc90 = 120, Confidence = Confidence.Moderate });
+risk1.Add(new LossMagnitude() { Perc10 = 100000, Mode = 500000, Perc90 = 20000000, Confidence = Confidence.Moderate});
 
 var risk2 = model.AddRisk("Risk 2: Data loss due to hardware failure.");
-risk2.Add(new LossEventFrequency() { Perc10 = 1, Mode = 2, Perc90 = 10, Confidence = QRiskTree.Engine.Confidence.High });
-risk2.Add(new LossMagnitude() { Perc10 = 100000, Mode = 200000, Perc90 = 1000000, Confidence = QRiskTree.Engine.Confidence.Moderate });
+risk2.Add(new LossEventFrequency() { Perc10 = 1, Mode = 2, Perc90 = 10, Confidence = Confidence.High });
+risk2.Add(new LossMagnitude() { Perc10 = 100000, Mode = 200000, Perc90 = 1000000, Confidence = Confidence.Moderate });
 
 var risk3 = model.AddRisk("Risk 3: Phishing attack leading to credential theft.");
-risk3.Add(new LossEventFrequency() { Perc10 = 12, Mode = 60, Perc90 = 1200, Confidence = QRiskTree.Engine.Confidence.Moderate });
-risk3.Add(new LossMagnitude() { Perc10 = 100, Mode = 500, Perc90 = 1500, Confidence = QRiskTree.Engine.Confidence.Low });
+risk3.Add(new LossEventFrequency() { Perc10 = 12, Mode = 60, Perc90 = 1200, Confidence = Confidence.Moderate });
+risk3.Add(new LossMagnitude() { Perc10 = 100, Mode = 500, Perc90 = 1500, Confidence = Confidence.Low });
 
 var mitigation1 = model.AddMitigation("Mitigation 1: Implement multi-factor authentication.");
 mitigation1.Perc10 = 5000;
 mitigation1.Mode = 10000;
 mitigation1.Perc90 = 20000;
-mitigation1.Confidence = QRiskTree.Engine.Confidence.Moderate;
-mitigation1.OperationCosts = new QRiskTree.Engine.Range(QRiskTree.Engine.RangeType.Money, 
-    1200, 6000, 20000, QRiskTree.Engine.Confidence.Moderate);
+mitigation1.Confidence = Confidence.Moderate;
+mitigation1.OperationCosts = new Range(RangeType.Money, 
+    1200, 6000, 20000, Confidence.Moderate);
 if (risk1.ApplyMitigation(mitigation1, out var r1Mitigation1))
 {
     r1Mitigation1.Perc10 = 0.1;
     r1Mitigation1.Mode = 0.2;
     r1Mitigation1.Perc90 = 0.5;
-    r1Mitigation1.Confidence = QRiskTree.Engine.Confidence.Moderate;
+    r1Mitigation1.Confidence = Confidence.Moderate;
 }
 if (risk3.ApplyMitigation(mitigation1, out var r3Mitigation1))
 {
     r3Mitigation1.Perc10 = 0.5;
     r3Mitigation1.Mode = 0.75;
     r3Mitigation1.Perc90 = 0.9;
-    r3Mitigation1.Confidence = QRiskTree.Engine.Confidence.Moderate;
+    r3Mitigation1.Confidence = Confidence.Moderate;
 }
 
 var mitigation2 = model.AddMitigation("Mitigation 2: Regular data backups.");
 mitigation2.Perc10 = 5000;
 mitigation2.Mode = 10000;
 mitigation2.Perc90 = 20000;
-mitigation2.Confidence = QRiskTree.Engine.Confidence.High;
-mitigation2.OperationCosts = new QRiskTree.Engine.Range(QRiskTree.Engine.RangeType.Money,
-    1200, 12000, 24000, QRiskTree.Engine.Confidence.Moderate);
+mitigation2.Confidence = Confidence.High;
+mitigation2.OperationCosts = new Range(RangeType.Money,
+    1200, 12000, 24000, Confidence.Moderate);
 if (risk2.ApplyMitigation(mitigation2, out var r2Mitigation2))
 {
     r2Mitigation2.Perc10 = 0.3;
     r2Mitigation2.Mode = 0.67;
     r2Mitigation2.Perc90 = 0.9;
-    r2Mitigation2.Confidence = QRiskTree.Engine.Confidence.High;
+    r2Mitigation2.Confidence = Confidence.High;
 }
 
 var mitigation3 = model.AddMitigation("Mitigation 3: Employee training on phishing awareness.");
 mitigation3.Perc10 = 2000000;
 mitigation3.Mode = 3000000;
 mitigation3.Perc90 = 50000000;
-mitigation3.Confidence = QRiskTree.Engine.Confidence.Moderate;
-mitigation1.OperationCosts = new QRiskTree.Engine.Range(QRiskTree.Engine.RangeType.Money,
-    10000, 20000, 50000, QRiskTree.Engine.Confidence.Moderate);
+mitigation3.Confidence = Confidence.Moderate;
+mitigation1.OperationCosts = new Range(RangeType.Money,
+    10000, 20000, 50000, Confidence.Moderate);
 if (risk3.ApplyMitigation(mitigation3, out var r3Mitigation3))
 {
     r3Mitigation3.Perc10 = 0.0001;
     r3Mitigation3.Mode = 0.00025;
     r3Mitigation3.Perc90 = 0.0004;
-    r3Mitigation3.Confidence = QRiskTree.Engine.Confidence.Moderate;
+    r3Mitigation3.Confidence = Confidence.Moderate;
 }
 
 uint iterations = Node.DefaultIterations;
