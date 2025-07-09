1db4c0587575daf65303f676056e9a65634fc000
Simone Curzi <simonecurzi@hotmail.com>
2025-07-09 08:30:52 +0200

    Added Undefined value as default.

--- QRiskTree.Engine/Model/ContactType.cs
+++ QRiskTree.Engine/Model/ContactType.cs
@@ -6,6 +6,13 @@
     public enum ContactType
     {
         /// <summary>
+        /// Represents an undefined state or value.
+        /// </summary>
+        /// <remarks>This member is used to indicate a state or value that has not been defined or
+        /// initialized. It can be useful in scenarios where a default or placeholder value is needed.</remarks>
+        Undefined,
+
+        /// <summary>
         /// A random contact happens by chance. There is no intentionality.
         /// </summary>
         Random,
